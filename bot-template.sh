4bcb46717e7a76efc28b61195da8357dd154fec0
engli-man <140743128+engli-man@users.noreply.github.com>
2025-03-28 18:10:28 +0300

    Created bot-template.sh

---
+++ bot-template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+touch .env
+touch .env.example
+touch .gitignore
+touch main.py
+mkdir config_data
+touch config_data/config.py
+mkdir filters
+touch filters/filters.py
+mkdir handlers
+touch handlers/other.py
+touch handlers/user.py
+mkdir lexicon
+touch lexicon/lexicon.py
+mkdir middlewares
+touch middlewares/inner.py
+touch middlewares/outer.py
